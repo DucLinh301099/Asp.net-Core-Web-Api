9d710a7907dfb00847ba535dbe995c3d63413496
vdlinh <vdlinh@ttanh2-thin.rd.local>
2024-05-21 10:26:40 +0700

    Update ngay 21 /5

---
+++ asp.net web api/asp.net web api/Controllers/ProductController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using asp.net_web_api.Models;
+
+namespace asp.net_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public static List<product> products = new List<product>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var product = products.SingleOrDefault(hh => hh.Id == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return Ok(product);
+            }
+            catch 
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        public IActionResult Create(productVM productVM) 
+        {
+            var product = new product
+            {
+                Id = Guid.NewGuid(),
+                Name = productVM.Name,
+                Description = productVM.Description,
+                Price = productVM.Price
+            };
+            products.Add(product);
+            return Ok(new
+            {
+                Success = true,
+                Data = product
+            });
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, product productedit) 
+        {
+            try
+            {
+                var product = products.SingleOrDefault(hh => hh.Id == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                if ( id != product.Id.ToString())
+                {
+                    return BadRequest();
+                }
+                product.Name = productedit.Name;
+                product.Description = productedit.Description;
+                product.Price = productedit.Price;
+                return  Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id) {
+            try
+            {
+                var product = products.SingleOrDefault(hh => hh.Id == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                if (id != product.Id.ToString())
+                {
+                    return BadRequest();
+                }
+                products.Remove(product);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ asp.net web api/asp.net web api/Models/product.cs
@@ -0,0 +1,15 @@
+﻿namespace asp.net_web_api.Models
+{
+    public class productVM
+    {
+        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }   
+
+    }
+    public class product : productVM
+    {
+        public Guid Id { get; set; }
+    }
+}
